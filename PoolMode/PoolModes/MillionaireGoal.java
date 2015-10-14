7ba315e9c4ad02e1f7c54d83164735ab3166de2d
unknown <is693706@iteso.mx>
2015-10-13 22:43:32 -0500

    As a pool organizer I want to select the pool mode so that I can play it

---
+++ PoolMode/PoolModes/MillionaireGoal.java
@@ -0,0 +1,10 @@
+
+public class MillionaireGoal extends PoolMode {
+	
+	public MillionaireGoal (){
+		this.description = "Millionaire goal mode";
+		this.info = "Por $5 pesos más, participas en la modalidad de Gol Millonario, \nen la que tienes que atinarle al número total de goles de los 12 partidos de la quiniela.";
+	}
+	
+
+}
---
+++ PoolMode/PoolModes/Regular.java
@@ -0,0 +1,11 @@
+
+public class Regular extends PoolMode {
+
+	public Regular (){
+		this.description = "Regular mode";
+		this.info = "Por sólo $15 pesos, rellena la casilla del equipo que crees \nque ganará en cada uno de los 12 partidos de la quiniela o si será empate.";
+	}
+	
+	
+	
+}
