e70824267952ea9c7e6e6c45a8aa5891ca261cc0
AldoRuizPimentel <is692909@iteso.mx>
2015-11-26 21:57:46 -0600

    Completed tests for quiniela class

--- main/com/iteso/quinielero/quiniela/Quiniela.java
+++ main/com/iteso/quinielero/quiniela/Quiniela.java
@@ -1,7 +1,9 @@
 package com.iteso.quinielero.quiniela;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import com.iteso.quinielero.users.Profile;
 
@@ -18,16 +20,11 @@ public class Quiniela {
 	private int minimum_participants;
 	private int creatorId;	
 	private List<Team> teams;
-	private String type = "";	
 	private ArrayList<Profile> pending_people = new ArrayList<Profile>();
 	private ArrayList<Profile> participants = new ArrayList<Profile>();
 
-	public Quiniela() { /// UN CONSTRUCTOR
-		this.type = "default";
-		teams = new ArrayList<Team>();
-	}
-
 	public Quiniela(String name) { ///// DOS CONSTRUCTORES???
+		teams = new ArrayList<Team>();
 		setName(name);
 	}
 
@@ -35,7 +32,6 @@ public class Quiniela {
 	public String getStart_date(){return start_date;}
 	public String getPoolMode(){return poolmode;}
 	public String getName(){return name;}
-	public String getType(){return type;}	
 	public int getMaximum_participants(){return maximum_participants;}
 	public int getMinimum_participants(){return minimum_participants;}	
 	public double getPools_price(){return pools_price;}	
@@ -49,55 +45,71 @@ public class Quiniela {
 	public void setLeagueName(String leagueName) {
 		if (leagueName == null || leagueName == "")
 			this.leagueName = "ERROR";
-		this.leagueName = leagueName;
+		else
+			this.leagueName = leagueName;
 	}
 
 	public void setMaximum_participants(int maximum_participants) {
-
 		if (maximum_participants > 99 || maximum_participants < 2)
 			this.maximum_participants = -1;
-		if (maximum_participants < getMinimum_participants())
+		else if (maximum_participants < getMinimum_participants())
 			this.minimum_participants = -1;
-		this.maximum_participants = maximum_participants;
+		else
+			this.maximum_participants = maximum_participants;
 	}
 
 	public void setMinimum_participants(int minimum_participants) {
 		if (maximum_participants < getMinimum_participants())
 			this.minimum_participants = -1;
-		if (minimum_participants > 99 || minimum_participants < 2)
+		else if (minimum_participants > 99 || minimum_participants < 2)
 			this.minimum_participants = -1;
-		this.minimum_participants = minimum_participants;
+		else
+			this.minimum_participants = minimum_participants;
 	}
 
 	private void setName(String name) {
 		if (name == "" || name == null)
 			this.name = "ERROR";
-		this.name = name;
-
-		getName();
+		else
+			this.name = name;
 	}
 
 	public void setPoolMode(String mode) {
 		if (mode == null || mode == "")
 			this.poolmode = "ERROR";
-		this.poolmode = mode;
+		else
+			this.poolmode = mode;
 	}
 
 	public void setPools_price(double pools_price) {
 
 		if (pools_price < 5)
 			this.pools_price = -1;
-		this.pools_price = pools_price;
+		else
+			this.pools_price = pools_price;
 	}
 
-	public void setStart_date(String start_date) {
-
-		this.start_date = start_date;
+	public void setStart_date(String sd) {
+		
+		
+		start_date = "ERROR";
+		
+		if(Character.isDigit(sd.charAt(0)))
+			if(Character.isDigit(sd.charAt(1)))
+				if(Character.isDigit(sd.charAt(2)))
+					if(Character.isDigit(sd.charAt(3)))
+						if(sd.charAt(4) == '-')
+							if(Character.isDigit(sd.charAt(5)))
+								if(Character.isDigit(sd.charAt(6)))
+									if(sd.charAt(7) == '-')
+										if(Character.isDigit(sd.charAt(8)))
+											if(Character.isDigit(sd.charAt(9)))
+												start_date = sd;
+		
+											
+		
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
 	
 	public void addTeam(Team team) {teams.add(team);}	
 
@@ -108,9 +120,8 @@ public class Quiniela {
 		for (Team myTeam : this.teams) {
 			teams += myTeam.toString() + " ";
 		}
-		return "Quiniela de tipo " + type + " con los equipos: " + teams;
+		return "Quiniela de tipo " + getPoolMode() + " con los equipos: " + teams;
 	}
-
 	
 
 	public void addPending_people(Profile profile) {pending_people.add(profile);}	
--- test/com/iteso/quinielero/quiniela/QuinielaTest.java
+++ test/com/iteso/quinielero/quiniela/QuinielaTest.java
@@ -1,128 +1,220 @@
 package com.iteso.quinielero.quiniela;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.*;
 import com.iteso.quinielero.quiniela.Quiniela;
 
 public class QuinielaTest {
 	String name;
 	Quiniela quiniela;
 
-	@Test
-	public void testQuinielaName() {
+	@Before
+	public void setUp(){
 		name = "quiniela";
 		quiniela = new Quiniela(name);
-		assertEquals("quiniela", quiniela.getName());
+	}	
+	
+	
+	//////////////////////////////////////////////////////
+	////////////////////// TEST NAME//////////////////////
+	//////////////////////////////////////////////////////
+	@Test
+	public void testQuinielaName() {
+		Quiniela q = new Quiniela(name);
+		assertEquals("quiniela", q.getName());
 	}
-
 	@Test
 	public void testQuinielaNameError() {
 		name = "";
-		quiniela = new Quiniela(name);
-		assertEquals("ERROR", quiniela.getName());
+		Quiniela q = new Quiniela(name);
+		assertEquals("ERROR", q.getName());
 	}
-
 	@Test
-	public void testQuinielaErrorNull() {
+	public void testQuinielaNameErrorNull() {
 		name = null;
-		quiniela = new Quiniela(name);
-		assertEquals("ERROR", quiniela.getName());
+		Quiniela q = new Quiniela(name);
+		assertEquals("ERROR", q.getName());
 	}
-
+	
+	
+	
+	//////////////////////////////////////////////////////
+	//////////////// TEST LEAGUE NAME ////////////////////
+	//////////////////////////////////////////////////////
 	@Test
-	public void setLeague() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
+	public void setLeagueName(){
 		quiniela.setLeagueName("UEFA");
 		assertEquals("UEFA", quiniela.getLeagueName());
 	}
-
 	@Test
-	public void testLeagueError() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
+	public void setLeagueNameError(){
 		quiniela.setLeagueName("");
-		assertEquals("ERROR", quiniela.getLeagueName());
+		assertEquals("ERROR", quiniela.getLeagueName());		
 	}
-
 	@Test
-	public void testLeagueNull() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
+	public void setLeagueNameErrorNull(){
 		quiniela.setLeagueName(null);
 		assertEquals("ERROR", quiniela.getLeagueName());
 	}
+	
 
+	//////////////////////////////////////////////////////
+	////////////////// TEST LEAGUE ID ////////////////////
+	//////////////////////////////////////////////////////
+	@Test
+	public void setLeagueId(){
+		quiniela.setLeagueId(2);
+		assertEquals(2, quiniela.getLeagueId());
+	}
+	
+	@Test
+	public void setLeagueIdError(){
+		quiniela.setLeagueId(0);
+		assertEquals(-1, quiniela.getLeagueId());		
+	}	
+	
+	
+
+	//////////////////////////////////////////////////////
+	////////////// TEST MAXIMUM MEMBERS///////////////////
+	//////////////////////////////////////////////////////	
 	@Test
 	public void testMaximum() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setMaximum_participants(10);
 		assertEquals(10, quiniela.getMaximum_participants());
 	}
-
 	@Test
 	public void testMaximumErrorExceedLimit() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
-		quiniela.setMaximum_participants(100);
+		quiniela.setMaximum_participants(100);		
 		assertEquals(-1, quiniela.getMaximum_participants());
 	}
-
 	public void testMaximumErrorExceedMinimum() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setMinimum_participants(10);
 		quiniela.setMaximum_participants(5);
 		assertEquals(-1, quiniela.getMaximum_participants());
 	}
+	
+	
 
+	//////////////////////////////////////////////////////
+	////////////// TEST MINIMUM MEMBERS///////////////////
+	//////////////////////////////////////////////////////	
 	@Test
 	public void testMinimum() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setMinimum_participants(10);
 		assertEquals(10, quiniela.getMinimum_participants());
 	}
-
 	@Test
 	public void testMinimumErrorExceedLimit() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setMinimum_participants(100);
 		assertEquals(-1, quiniela.getMinimum_participants());
 	}
-
 	public void testMinimumErrorExceedMinimum() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setMinimum_participants(10);
 		quiniela.setMaximum_participants(5);
 		assertEquals(-1, quiniela.getMinimum_participants());
 	}
-
+	
+	
+	//////////////////////////////////////////////////////
+	//////////////////TEST POOL PRICE/////////////////////
+	//////////////////////////////////////////////////////	
 	@Test
-	public void testPoolPrice() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
+	public void testPoolPriceInt() {
 		quiniela.setPools_price(6);
 		assertEquals(6, quiniela.getPools_price(), 0);
 	}
-
 	@Test
 	public void testPoolPriceFloat() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setPools_price(6.5);
 		assertEquals(6.5, quiniela.getPools_price(), 0);
 	}
-
 	@Test
 	public void testPoolPriceErrorMinimum() {
-		name = "quiniela";
-		quiniela = new Quiniela(name);
 		quiniela.setPools_price(2);
 		assertEquals(-1, quiniela.getPools_price(), 0);
-
 	}
+	
+	
+	//////////////////////////////////////////////////////
+	///////////////////TEST POOL MODE/////////////////////
+	//////////////////////////////////////////////////////	
+	@Test
+	public void testPoolMode(){
+		quiniela.setPoolMode("Jornada");
+		assertEquals("Jornada", quiniela.getPoolMode());
+	}
+	@Test
+	public void testPoolModeError(){
+		quiniela.setPoolMode("");
+		assertEquals("ERROR", quiniela.getPoolMode());
+	}
+	@Test
+	public void testPoolModeErrorNull(){
+		quiniela.setPoolMode(null);
+		assertEquals("ERROR", quiniela.getPoolMode());
+	}
+	
+	
+	
+	//////////////////////////////////////////////////////
+	//////////////////TEST POOL MODE ID///////////////////
+	//////////////////////////////////////////////////////	
+	@Test
+	public void testPoolModeId(){
+		quiniela.setPoolModeId(2);
+		assertEquals(2, quiniela.getPoolModeId());
+	}
+	@Test
+	public void testPoolModeIdError(){
+		quiniela.setPoolModeId(0);
+		assertEquals(-1, quiniela.getPoolModeId());
+	}
+	
+	
+	//////////////////////////////////////////////////////
+	////////////////////TEST CREATOR ID///////////////////
+	//////////////////////////////////////////////////////	
+	@Test
+	public void testCreatorId(){
+		quiniela.setCreatorId(3);
+		assertEquals(3, quiniela.getCreatorId());
+	}
+	@Test
+		public void testCreatorIdError(){
+		quiniela.setCreatorId(0);
+		assertEquals(-1, quiniela.getCreatorId());
+	}
+	
+	//////////////////////////////////////////////////////
+	////////////////////TEST START DATE///////////////////
+	//////////////////////////////////////////////////////	
+	@Test
+	public void testStartDate(){
+		quiniela.setStart_date("2015-12-12");
+		assertEquals("2015-12-12", quiniela.getStart_date());
+	}
+	@Test
+	public void testStartDateError1(){
+		quiniela.setStart_date("fecha mal ingresada");
+		assertEquals("ERROR", quiniela.getStart_date());
+	}
+	@Test
+	public void testStartDateError(){
+		quiniela.setStart_date("12-12-2015");
+		assertEquals("ERROR", quiniela.getStart_date());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
